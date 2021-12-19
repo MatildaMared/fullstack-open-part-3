4fd8ad192a4fa898ae14e5fdbc8acb0bc026eefa
Matilda Mared <matildamared@live.se>
2021-12-19 10:02:11 +0100

    complete exercise 3.3

--- index.js
+++ index.js
@@ -41,6 +41,18 @@ app.get("/api/persons", (req, res) => {
 	res.status(200).json(persons);
 });
 
+// GET single person by id
+app.get("/api/persons/:id", (req, res) => {
+	const id = Number(req.params.id);
+	const person = persons.find((person) => person.id === id);
+
+	if (person) {
+		res.status(200).json(person);
+	} else {
+		res.status(404).end();
+	}
+});
+
 const PORT = 3001;
 
 app.listen(PORT, () => {
